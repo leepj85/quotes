7330dfc3d8ba31f53dc6e166e60f911039c4782a
Padmapriya Ganapathi <gpadmapriya@hotmail.com>
2019-08-15 12:49:01 -0700

    Added tests

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# quotes
+### Quotes
+####Feature Task
+- Use quotes file provided (src/main/resources/quotes.json) and display a random quote and author from the file each time the application is run.
+- Use GSON to parse the json file
+
+#### Approach
+- Create a Quotes class
+- Instantiate a FileReader object to read from file
+- Instantiate a JsonReader object to read the file as a stream of JSON tokens
+- Use the instantiated JsonReader and the structure of Quotes class to generate an array of quotes objects
+- Generate a random number and use it as the index of the quotes array to return a random quote each time the app is run
+
+#### To test
+- Add the following dependency to the dependencies in build.gradle file
+  - implementation 'com.google.code.gson:gson:2.8.5' 
+  - Import/reconfigure all gradle projects to use this dependency
+  - Check if following packages are imported and available
+    - com.google.gson.Gson
+    - com.google.gson.stream.JsonReader
+    - java.io.FileNotFoundException
+    - java.io.FileReader;
+
+#### Colloborators
+- Peter Lee
+- Promila Tamang
+- Padma Ganapathi
--- src/test/java/quotes/AppTest.java
+++ src/test/java/quotes/AppTest.java
@@ -3,12 +3,52 @@
  */
 package quotes;
 
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test public void testAppHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+
+    @Test
+    public void testQuoteHasAnAuthor() throws FileNotFoundException {
+        String filePath = "src/main/resources/quotes.json";
+        String newQuote = App.returnQuote(filePath);
+        assertTrue(newQuote.contains("Author"));
     }
+    @Test
+    public void testLengthOfReturnedArray() throws FileNotFoundException {
+        String filePath = "src/main/resources/quotes.json";
+        Gson gson = new Gson();
+        JsonReader reader = new JsonReader(new FileReader(filePath));
+        Quote[] quotes = gson.fromJson(reader, Quote[].class);
+        assertEquals(138,quotes.length);
+    }
+    @Test
+    public void testQuoteHasText() throws FileNotFoundException {
+        String filePath = "src/main/resources/quotes.json";
+        String newQuote = App.returnQuote(filePath);
+        assertTrue(newQuote.contains("Quote"));
+        newQuote.
+
+    }
+    @Test
+    public void testRandomNumberGenerator() throws FileNotFoundException {
+        String filePath = "src/main/resources/quotes.json";
+        Gson gson = new Gson();
+        JsonReader reader = new JsonReader(new FileReader(filePath));
+        Quote[] quotes = gson.fromJson(reader, Quote[].class);
+        int newRandom = (int) (Math.random() * quotes.length);
+        int high = quotes.length - 1;
+        int low = 0;
+        assertTrue("Error, random is too high", high >= newRandom);
+        assertTrue("Error, random is too low",  low  <= newRandom);
+
+    }
+
 }
